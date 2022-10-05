95e525f1e0db4ec35273e7175e08f2f66aade2c4
rayhan926 <rayhanbd7075@gmail.com>
2022-10-05 15:10:40 +0600

    Add black bg

--- src/views/Farm/index.tsx
+++ src/views/Farm/index.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Farm = () => {
-  return <div>Farm</div>;
+  return (
+    <>
+      {/** Top section --Start-- */}
+      <section>
+        <h1 className="text-[46px] leading-[46px]">
+          You don’t have available assets to farm in your wallet.
+        </h1>
+        <p>
+          Stargate liquidity providers can farm their LP tokens in exchange for
+          STG rewards. Earn STG and become a member of the Stargate community.
+        </p>
+      </section>
+      {/** Top section --End-- */}
+    </>
+  );
 };
 
 export default Farm;
